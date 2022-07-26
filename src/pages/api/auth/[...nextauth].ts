9904c326f810f554d1d8bb41a57eff9a1ee42aaa
Anders Gustafsson <andersgee@gmail.com>
2022-07-26 23:42:42 +0200

    auth/signin page

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -9,6 +9,9 @@ import { prisma } from "../../../server/db/client";
 import { env } from "../../../env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/auth/signin",
+  },
   // Include user.id on session
   callbacks: {
     session({ session, user }) {
